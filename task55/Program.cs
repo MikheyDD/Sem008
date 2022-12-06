2f63141d47d2d0115935b6dfd6e0e75549680a3d
Mikheyev_Denis <mikheydd@mail.ru>
2022-12-06 22:24:49 +0300

    Sem008

---
+++ task55/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести 
+// сообщение для пользователя.
+
+int[,] arr = FillMatrix(3, 3);
+PrintMatrix(arr);
+var result = ChangeRows(arr);
+System.Console.WriteLine();
+PrintMatrix(result);
+
+
+int[,] ChangeRows(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int colums = matrix.GetLength(1);
+    if (rows != colums)
+    {
+        return null;
+    }
+    int[,] result = new int[rows, colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            result[j, i] = matrix[i, j];
+        }
+    }
+    return result;
+}
+
+int[,] FillMatrix(int rows, int colums)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 6);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
